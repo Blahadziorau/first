626bcd69b104aa09ab3d4cd46cf7c6ab4dd529d4
Blahadziorau <rollerxander@gmail.com>
2016-05-11 19:37:20 +0300

    Update MyFirstClass.java

--- src/com/gmail/rollerxander/first/MyFirstClass.java
+++ src/com/gmail/rollerxander/first/MyFirstClass.java
@@ -3,7 +3,7 @@ package com.gmail.rollerxander.first;
 public class MyFirstClass {
 
     public static void main(String[] args) {
-
-         System.out.println(resul);
+     System.out.println(result);
+         System.out.println(result);
     }
 }
