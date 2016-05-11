409da27064bf5e94a678a2a577d0be81c5dd9ab5
Blahadziorau <rollerxander@gmail.com>
2016-05-11 21:51:20 +0300

    1

--- src/com/gmail/rollerxander/first/Second.java
+++ src/com/gmail/rollerxander/first/Second.java
@@ -1,7 +1,16 @@
 package com.gmail.rollerxander.first;
 
 public class Second {
-    public  static void main(String... args){
+    public static void main(String... args) {
 
+        for (int i=1000; i > 999;i+=3 ) {
+
+            System.out.println(i);
+           if(i>2000){
+               break;
+           }
+        }
     }
+
 }
+
