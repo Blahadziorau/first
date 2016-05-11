c787f80c497e9afe534f6668b8cc522deb809976
Blahadziorau <rollerxander@gmail.com>
2016-05-11 21:29:55 +0300

    last

--- src/com/gmail/rollerxander/first/MyFirstClass.java
+++ src/com/gmail/rollerxander/first/MyFirstClass.java
@@ -1,9 +1,44 @@
 package com.gmail.rollerxander.first;
 
+import java.util.Scanner;
+
 public class MyFirstClass {
 
     public static void main(String[] args) {
-            System.out.println(result);
-         System.out.println(result);
+        int number;
+
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.println("Введи число");
+            number = scanner.nextInt();
+
+            switch (number) {
+                case 3:
+                case 4:
+                case 5:
+                    System.out.println("Vesna");
+                    break;
+                case 1:
+                case 2:
+                case 12:
+                    System.out.println("Zima");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    System.out.println("Leto");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    System.out.println("Leto");
+                    break;
+                default:
+                    System.out.println("vvedi ot 1 do 12");
+            }
+        } while (number >= 1 && number <= 12) ;
+
+
+
     }
 }
---
+++ src/com/gmail/rollerxander/first/Second.java
@@ -0,0 +1,7 @@
+package com.gmail.rollerxander.first;
+
+public class Second {
+    public  static void main(String... args){
+
+    }
+}
