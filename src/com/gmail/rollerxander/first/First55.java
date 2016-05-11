49babaa05f3a00386ac062a24812ad9db6d6fbd4
Blahadziorau <rollerxander@gmail.com>
2016-05-11 22:09:49 +0300

    1

--- src/com/gmail/rollerxander/first/First55.java
+++ src/com/gmail/rollerxander/first/First55.java
@@ -5,7 +5,7 @@ public class First55 {
     public static void main(String... args){
     int count55=0;
         for (int i=0; count55<56;i++){
-            if(i%0){
+            if(i%2!=0){
                 System.out.println(i);
             }
             count55++;
