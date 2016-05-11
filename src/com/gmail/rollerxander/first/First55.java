c9814b794b55d001b458b631cfb993fded8eea3a
Blahadziorau <rollerxander@gmail.com>
2016-05-11 22:02:38 +0300

    1

---
+++ src/com/gmail/rollerxander/first/First55.java
@@ -0,0 +1,14 @@
+package com.gmail.rollerxander.first;
+
+
+public class First55 {
+    public static void main(String... args){
+    int count55=0;
+        for (int i=0; count55<56;i++){
+            if(i%0){
+                System.out.println(i);
+            }
+            count55++;
+        }
+    }
+}
