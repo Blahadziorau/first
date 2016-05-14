20abd0efcc28664d66cd071f02423c03bb00dfd1
Rollershop <rollerxander@gmail.com>
2016-05-14 15:28:38 +0300

    15:28 (14 may)

---
+++ src/com/gmail/rollerxander/first/FactorialByScanner.java
@@ -0,0 +1,17 @@
+package com.gmail.rollerxander.first;
+
+/*программа, вычисляющая факториал натурального числа n, которое пользователь введёт с клавиатуры.  */
+
+import java.util.Scanner;
+
+public class FactorialByScanner {
+    public static void main(String... args){
+        System.out.println("Введи натуральное число");
+        Scanner sc=new Scanner(System.in);
+        long n=sc.nextLong();
+
+
+    }
+
+
+}
