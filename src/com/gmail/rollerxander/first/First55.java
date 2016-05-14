343c0472651b42c5bcd26b89c5eb860c5e5b2bc7
Rollershop <rollerxander@gmail.com>
2016-05-14 14:26:50 +0300

    14:26 (14 may)

--- src/com/gmail/rollerxander/first/First55.java
+++ src/com/gmail/rollerxander/first/First55.java
@@ -1,5 +1,6 @@
 package com.gmail.rollerxander.first;
 
+/*программа, выводящая на экран первые 55 элементов последовательности 1 3 5 7 9 11 13 15 17 ….*/
 
 public class First55 {
     public static void main(String... args){
