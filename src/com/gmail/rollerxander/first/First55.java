901d0f8aadd75e66c5d4bcdbb3d95adf53a871eb
Rollershop <rollerxander@gmail.com>
2016-05-14 13:57:04 +0300

    13:56  (14 may)

--- src/com/gmail/rollerxander/first/First55.java
+++ src/com/gmail/rollerxander/first/First55.java
@@ -5,7 +5,7 @@ public class First55 {
     public static void main(String... args){
     int count55=1;
         for (int i=0; ;i++){
-            if(i%2!=0&count55<56){
+            if(i%2!=0){
                 System.out.println(i);
                 count55++;
             }else if (count55==56){
