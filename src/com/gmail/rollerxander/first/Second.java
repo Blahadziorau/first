e959cabee7627a047e2f5eb90ce7b0b14b567989
Rollershop <rollerxander@gmail.com>
2016-05-14 14:15:09 +0300

    14:15  (14 may)

--- src/com/gmail/rollerxander/first/Second.java
+++ src/com/gmail/rollerxander/first/Second.java
@@ -3,12 +3,12 @@ package com.gmail.rollerxander.first;
 public class Second {
     public static void main(String... args) {
 
-        for (int i=1000; i > 999;i+=3 ) {
+        for (int i=1000; i < 10000;i+=3 ) {
 
             System.out.println(i);
-           if(i>2000){
+          /* if(i>2000){
                break;
-           }
+           }*/
         }
     }
 
