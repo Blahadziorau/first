4e4122123661d8d6313fb030b27b6c07fd47ed42
Rollershop <rollerxander@gmail.com>
2016-05-14 15:41:20 +0300

    15:41 (14 may)

--- src/com/gmail/rollerxander/first/FactorialByScanner.java
+++ src/com/gmail/rollerxander/first/FactorialByScanner.java
@@ -6,11 +6,20 @@ import java.util.Scanner;
 
 public class FactorialByScanner {
     public static void main(String... args){
-        System.out.println("Введи натуральное число");
-        Scanner sc=new Scanner(System.in);
-        long n=sc.nextLong();
-
-
+        int n;
+        do {
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Введите факториал натурально числа n : ");
+            n = scan.nextInt();
+            int result = 1;
+            for (int i = 2; i <= n; i++)
+                result *= i;
+            System.out.println(result);
+            if (n == 0 || n == 1)
+                System.out.println(result);
+            if(n < 0)
+                System.out.println("Вы ввели отрицательный факториал!");
+        } while (n < 0);{System.out.println("Конец");}
     }
 
 
