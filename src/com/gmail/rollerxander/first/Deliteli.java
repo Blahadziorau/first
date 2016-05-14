4e788f705b5ca2f40602cdeae185d9d1247d1ef9
Rollershop <rollerxander@gmail.com>
2016-05-14 15:53:42 +0300

    15:51 (14 may)

---
+++ src/com/gmail/rollerxander/first/Deliteli.java
@@ -0,0 +1,9 @@
+package com.gmail.rollerxander.first;
+
+/*   положительные делители натурального числа, введённого пользователем с клавиатуры.
+ */
+public class Deliteli {
+    public static void main (String... args){
+
+    }
+}
