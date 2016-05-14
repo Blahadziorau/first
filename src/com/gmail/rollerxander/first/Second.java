c82184be439138f66e8ca797ddadc9985ffe46cf
Rollershop <rollerxander@gmail.com>
2016-05-14 14:25:15 +0300

    14:25  (14 may)

--- src/com/gmail/rollerxander/first/Second.java
+++ src/com/gmail/rollerxander/first/Second.java
@@ -1,5 +1,7 @@
 package com.gmail.rollerxander.first;
 
+ /*программа, выводящая на экран все четырёхзначные числа последовательности 1000 1003 1006 1009 1012 1015 ….*/
+
 public class Second {
     public static void main(String... args) {
 
