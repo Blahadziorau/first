3889ae009a2c071b5dea49eb6d125b6aa28e9721
Blahadziorau <rollerxander@gmail.com>
2016-05-16 20:34:14 +0300

    neregular2

--- src/com/gmail/rollerxander/first/SecondTaskList/Date16_05_2016/NeregularMassive.java
+++ src/com/gmail/rollerxander/first/SecondTaskList/Date16_05_2016/NeregularMassive.java
@@ -5,9 +5,8 @@ package com.gmail.rollerxander.first.SecondTaskList.Date16_05_2016;
  */
 public class NeregularMassive {
     public static void main(String... args) {
-        int[][] matrix = new int[5][];
+        int[][] matrix = new int[][]{{1},{1,2},{1,2,3},{1,2,3,4},{1,2,3,4,5}};
         for (int i = 0; i < matrix.length; i++) {
-            matrix[i]= new int[i+1];
 
             for (int t = 0; t < matrix[i].length; t++) {
                 System.out.print(matrix[i][t] + "; ");
