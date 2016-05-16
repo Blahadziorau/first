604c5669b27e432816f0c577061327765b9ec7c6
Blahadziorau <rollerxander@gmail.com>
2016-05-16 20:01:45 +0300

    1

--- src/com/gmail/rollerxander/first/SecondTaskList/Date16_05_2016/Massive.java
+++ src/com/gmail/rollerxander/first/SecondTaskList/Date16_05_2016/Massive.java
@@ -1,5 +1,8 @@
 package com.gmail.rollerxander.first.SecondTaskList.Date16_05_2016;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  * Created by USER on 16.05.2016.
  */
@@ -7,6 +10,16 @@ public class Massive {
     public static void main(String... args) {
         int[] mass = new int[10];
 
-        System.out.println();
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < mass.length; i++) {
+
+            while (!scanner.hasNextInt()) {
+                scanner.next();
+            }
+            mass[i] = scanner.nextInt();
+        }
+        System.out.println(Arrays.toString(mass));
+
     }
 }
