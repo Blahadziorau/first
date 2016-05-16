16a4903de0615655839dc68fc2b5048fb8314e59
Blahadziorau <rollerxander@gmail.com>
2016-05-16 21:52:27 +0300

    Data

--- src/com/gmail/rollerxander/first/SecondTaskList/Date16_05_2016/ClassData.java
+++ src/com/gmail/rollerxander/first/SecondTaskList/Date16_05_2016/ClassData.java
@@ -1,13 +1,16 @@
 package com.gmail.rollerxander.first.SecondTaskList.Date16_05_2016;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 /**
  * Created by USER on 16.05.2016.
  */
 public class ClassData {
-    public static void main(String... args){
+    public static void main(String... args) {
         Date date = new Date();
         System.out.println(date);
+        Calendar calendar = new GregorianCalendar();
     }
 }
--- src/com/gmail/rollerxander/first/SecondTaskList/Date16_05_2016/ClassMath.java
+++ src/com/gmail/rollerxander/first/SecondTaskList/Date16_05_2016/ClassMath.java
@@ -4,7 +4,7 @@ package com.gmail.rollerxander.first.SecondTaskList.Date16_05_2016;
  * Created by USER on 16.05.2016.
  */
 public class ClassMath {
-    public static  void main(String... args){
+    public static void main(String... args) {
         System.out.println(Math.PI);
         System.out.println(Math.E);
 
