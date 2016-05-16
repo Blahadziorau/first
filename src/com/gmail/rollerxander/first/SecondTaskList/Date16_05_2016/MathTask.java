e4705dba6a48e0e00329b73a1bca6a51b2227b60
Blahadziorau <rollerxander@gmail.com>
2016-05-16 22:09:31 +0300

    Площадь и периметр

---
+++ src/com/gmail/rollerxander/first/SecondTaskList/Date16_05_2016/MathTask.java
@@ -0,0 +1,12 @@
+package com.gmail.rollerxander.first.SecondTaskList.Date16_05_2016;
+
+/**
+ * Created by USER on 16.05.2016.
+ */
+public class MathTask {
+    public static void main (String... args){
+        double x=1,y=2;
+        System.out.println(x*y/2);
+        System.out.println(Math.hypot( x,  y )+x+y);
+    }
+}
