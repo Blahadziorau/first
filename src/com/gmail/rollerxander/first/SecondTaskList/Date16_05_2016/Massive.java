2c15dca0432a88dccf82853d36a5ff64ea05fa7f
Blahadziorau <rollerxander@gmail.com>
2016-05-16 19:36:55 +0300

    1

---
+++ src/com/gmail/rollerxander/first/SecondTaskList/Date16_05_2016/Massive.java
@@ -0,0 +1,12 @@
+package com.gmail.rollerxander.first.SecondTaskList.Date16_05_2016;
+
+/**
+ * Created by USER on 16.05.2016.
+ */
+public class Massive {
+    public static void main(String... args) {
+        int[] mass = new int[10];
+
+        System.out.println();
+    }
+}
