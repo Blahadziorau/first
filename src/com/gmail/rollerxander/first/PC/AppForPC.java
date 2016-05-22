ec131f2f9b2d629a961e2c55dd4fee4d43403f82
Rollershop <rollerxander@gmail.com>
2016-05-22 13:49:04 +0300

    18:41(21 may)

--- src/com/gmail/rollerxander/first/PC/AppForPC.java
+++ src/com/gmail/rollerxander/first/PC/AppForPC.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
  */
 public class AppForPC {
     public static void main(String[] args) throws IOException {
-        PersonalComputer pc1 = new PersonalComputer("ForOffice", new Monitor("lg", 1200, 800), new Cpu("Intel", 4, 2.5), new Hdd("WD", 500), new Ram("Kingston", 4096));
+        PersonalComputer pc1 = new PersonalComputer("ForOffice", new Monitor("lg", 1280, 800), new Cpu("Intel", 4, 2.5), new Hdd("WD", 500), new Ram("Kingston", 4096));
         pc1.PcTotalInfo();
         // System.out.println(pc1.getMonitor().toString()+"\n"+ pc1.getCpu().toString()+"\n"+pc1.getHdd().toString()+"\n"+pc1.getRam().toString());
 
