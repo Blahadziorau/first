fdd415ad8294b19773112fa9c3666cbb26672583
Blahadziorau <rollerxander@gmail.com>
2016-05-23 21:44:50 +0300

    1

--- src/com/gmail/rollerxander/first/date23052016/App.java
+++ src/com/gmail/rollerxander/first/date23052016/App.java
@@ -5,15 +5,15 @@ package com.gmail.rollerxander.first.date23052016;
  */
 public class App {
 
-    public static void main(String... args){
-        String s="";
-        long start=System.currentTimeMillis();
-        for (int i = 0; i < 50_000 ; i++) {
-            s+="a";
+    public static void main(String... args) {
+        String s = "";
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 50_000; i++) {
+            s += "a";
         }
-        long end= System.currentTimeMillis();
-        System.out.println(end-start);
-        if (s!=null){
+        long end = System.currentTimeMillis();
+        System.out.println(end - start);
+        if (s != null) {
 
         }
     }
