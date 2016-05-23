418703171b47cd5ff5a77034895a9d3b02232552
Blahadziorau <rollerxander@gmail.com>
2016-05-23 22:11:14 +0300

    1

--- src/com/gmail/rollerxander/first/date23052016/App.java
+++ src/com/gmail/rollerxander/first/date23052016/App.java
@@ -6,15 +6,11 @@ package com.gmail.rollerxander.first.date23052016;
 public class App {
 
     public static void main(String... args) {
-        String s = "";
-        long start = System.currentTimeMillis();
-        for (int i = 0; i < 50_000; i++) {
-            s += "a";
-        }
-        long end = System.currentTimeMillis();
-        System.out.println(end - start);
-        if (s != null) {
 
-        }
+        String s = "green fields";
+        System.out.println(s.contains("fields"));
+        System.out.println(s.indexOf("fields"));
+        System.out.println(s.lastIndexOf("e"));
+
     }
 }
