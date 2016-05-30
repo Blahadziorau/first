30d0fccc275508b203fe86e2790decb9a071e7ea
Blahadziorau <rollerxander@gmail.com>
2016-05-30 19:04:11 +0300

    1

---
+++ src/com/gmail/rollerxander/first/date30_05_2016/App30may.java
@@ -0,0 +1,10 @@
+package com.gmail.rollerxander.first.date30_05_2016;
+
+/**
+ * Created by USER on 30.05.2016.
+ */
+public class App30may {
+    public static void main(String... args){
+
+    }
+}
