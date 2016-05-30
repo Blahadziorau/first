1b5a4a696eecd120b4971fc679d6a565d59cdb1b
Blahadziorau <rollerxander@gmail.com>
2016-05-30 20:35:25 +0300

    1

--- src/com/gmail/rollerxander/first/date30_05_2016/App30may.java
+++ src/com/gmail/rollerxander/first/date30_05_2016/App30may.java
@@ -1,6 +1,5 @@
 package com.gmail.rollerxander.first.date30_05_2016;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
@@ -9,7 +8,7 @@ import java.util.Date;
 public class App30may {
     public static void main(String... args) {
 
-        Student s1 = new Student(1, "Ivanov", "Ivan", "Ivanovich", new Date(100, 10,31 ), 1, 25);
+        Student s1 = new Student(1, "Ivanov", "Ivan", "Ivanovich", new Date(100, 10, 31), 1, 25);
         Student s2 = new Student(2, "Petrov", "Petia", "Petrovich", new Date(105, 03, 16), 5, 4);
         Student s3 = new Student(3, "Sidorov", "Sidor", "Sidorovich", new Date(107, 10, 01), 3, 4);
         Student s4 = new Student(4, "Krasnow", "Kras", "Krasovich", new Date(101, 06, 21), 2, 4);
