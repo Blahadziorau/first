ce47d6c2621dc5e82aca5905daabb8224d2dcecb
Rollershop <rollerxander@gmail.com>
2016-06-12 17:37:51 +0300

    1

--- src/com/gmail/rollerxander/first/HomeWork/Home12_06/ArrList.java
+++ src/com/gmail/rollerxander/first/HomeWork/Home12_06/ArrList.java
@@ -1,6 +1,8 @@
 package com.gmail.rollerxander.first.HomeWork.Home12_06;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -14,7 +16,7 @@ public class ArrList {
         }
         System.out.println("Добавление прошло успешно!");
 
-        mass.add(5, new Element("Jubi", 1985));
+        mass.add(5, new Element("Jubi", 1988));
         // mass.remove(5);
 
 
@@ -24,6 +26,19 @@ public class ArrList {
             System.out.println(mass.get(el).toString());
         }
         System.out.println("Размер листа: " + mass.size());
+
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        map.put("Sasha", 1982);
+        map.put("Julia", 1988);
+        System.out.println(map.get("Sasha"));
+        System.out.println(map.size());
+        System.out.println(map.keySet());
+        System.out.println(map.values());
+        System.out.println();
+        Map<Integer, Element> mapElements = new HashMap<Integer, Element>();
+        mapElements.put(1, new Element("Petia", 2000));
+        mapElements.put(2, new Element("cat", 2001));
+        System.out.println(mapElements.get(2));
     }
 
 }
