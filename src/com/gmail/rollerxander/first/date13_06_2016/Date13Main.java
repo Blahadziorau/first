30c1721321dfa59ec99bded20525319bf99aef08
Blahadziorau <rollerxander@gmail.com>
2016-06-14 21:21:28 +0300

    1

--- src/com/gmail/rollerxander/first/date13_06_2016/Date13Main.java
+++ src/com/gmail/rollerxander/first/date13_06_2016/Date13Main.java
@@ -1,12 +1,6 @@
 package com.gmail.rollerxander.first.date13_06_2016;
 
-import com.gmail.rollerxander.first.date06_06_2016.CompareUserByName;
-import com.gmail.rollerxander.first.date08_06_2016.Collections.User;
-
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created by Java on 13.06.2016.
@@ -16,25 +10,47 @@ public class Date13Main {
 
     public static void main(String[] args) {
 
-        map.put("id", new IdUserComparator());
-        map.put("name", new NameUserComparator());
+//        map.put("id", new IdUserComparator());
+//         map.put("name", new NameUserComparator());
+//
+//        User[] users = new User[]{new User(2, "Sasha"), new User(5, "Dima"), new User(1, "Jason")};
+//        String key = args[0];
+//
+//        Comparator<User> comparator = map.get(key);
+//
+//        Map<Integer, User> map = new HashMap<>();
+//        map.put(1, new User(1, "Felix"));
+//        map.put(2, new User(1, "Telix"));
+//        map.put(3, new User(1, "Velix"));
 
-        User[] users=new User[]{new User(2,"Sasha"), new User(5,"Dima"), new User(1,"Jason")};
-        String key=args[0];
+//        Set<Integer> set = map.keySet();
+//        for (Map.Entry<Integer, User> entry : map.entrySet()) {
+//            System.out.println(entry.getValue());
+//        }
 
-        Comparator<User> comparator=map.get(key);
+//        Random random= new Random();
+//        List<User> integers=new ArrayList<>();
+//        for (int i = 0; i <100000 ; i++) {
+//            integers.add(new User(random.nextInt(10000), "aaaaa"));
+//        }
+//        System.out.println(integers);
+//        Collections.sort(integers,new IdUserComparator());
+//        System.out.println(integers);
+        int x = 10;
+        int x1 = 100;
+        int y = 10;
+        int y1 = 100;
 
-        Map<Integer, User> map= new HashMap<>();
-        map.put(1, new User(1,"Felix"));
-        map.put(2, new User(1,"Telix"));
-        map.put(3, new User(1,"Velix"));
-
-        Set<Integer> set = map.keySet();
-
-        for (Map.Entry<Integer,User>entry: map.entrySet()){
-            System.out.println(entry.getValue());
+        TreeSet<Point> points = new TreeSet<>((p1, p2) -> p1.getX() - p2.getX());
+        Random random = new Random();
+        for (int i = 0; i < 10000; i++) {
+            points.add(new Point(random.nextInt(1000), random.nextInt(1000)));
         }
+        TreeSet<Point> points1 = new TreeSet<>((p1, p2) -> p1.getY() - p2.getY());
+        points.subSet(new Point(x, 10), new Point(x1, 10));
 
+        Set<Point> points2 = points1.subSet(new Point(1, y), new Point(1, y1));
+        System.out.println(points2);
     }
 
 }
---
+++ src/com/gmail/rollerxander/first/date13_06_2016/Point.java
@@ -0,0 +1,30 @@
+package com.gmail.rollerxander.first.date13_06_2016;
+
+/**
+ * Created by Java on 13.06.2016.
+ */
+public final class Point {
+    private final int x;
+    private final int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
